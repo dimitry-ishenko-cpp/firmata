5aa68bd15955beaa08e4f5273ead4e86719fe3c7
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2017-10-05 16:11:01 -0400

    encoder: explain which pin is invalid

--- encoder.cpp
+++ encoder.cpp
@@ -18,13 +18,14 @@ namespace firmata
 ////////////////////////////////////////////////////////////////////////////////
 encoder::encoder(pin& pin1, pin& pin2) : pin1_(pin1), pin2_(pin2)
 {
-    if((pin1.mode() == digital_in || pin1.mode() == pullup_in)
-    && (pin2.mode() == digital_in || pin2.mode() == pullup_in))
-    {
-        using namespace std::placeholders;
-        id_ = pin1_.on_state_changed(std::bind(&encoder::pin_state_changed, this, _1));
-    }
-    else throw std::invalid_argument("Invalid pin mode");
+    if(pin1_.mode() != digital_in && pin1_.mode() != pullup_in)
+        throw std::invalid_argument("Invalid pin1 mode");
+
+    if(pin2_.mode() != digital_in && pin2_.mode() != pullup_in)
+        throw std::invalid_argument("Invalid pin2 mode");
+
+    using namespace std::placeholders;
+    id_ = pin1_.on_state_changed(std::bind(&encoder::pin_state_changed, this, _1));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
